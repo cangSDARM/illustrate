ae2d49eb1a0e27ed54032aa3dede8b6b302b5041
AllenLee <648384410li@gmail.com>
2023-05-19 14:27:39 +0800

    translate: serverEncryptionKeysGeneration, serverChangeCipherSpec and serverHandshakeFinished fix: #1

--- src/TLS12/index.js
+++ src/TLS12/index.js
@@ -113,6 +113,29 @@ const data = {
       label: "客户端握手完成",
       json: () => import("./clientHandshakeFinished.json"),
     },
+    {
+      type: "RecOuter",
+      tags: ["calculation", "server"],
+      label: "服务器端生成会话密钥",
+      illustration: {
+        src: "https://quic.xargs.org/images/key9.png",
+        width: "97",
+        height: "250",
+      },
+      json: () => import("./serverEncryptionKeysGeneration.json"),
+    },
+    {
+      type: "RecOuter",
+      tags: ["record", "server"],
+      label: "服务器端秘钥规格变更",
+      json: () => import("./serverChangeCipherSpec.json"),
+    },
+    {
+      type: "RecOuter",
+      tags: ["record", "server"],
+      label: "服务器端握手完成",
+      json: () => import("./serverHandshakeFinished.json"),
+    },
   ],
   ending: {
     mother: "https://tls12.xargs.org/",
